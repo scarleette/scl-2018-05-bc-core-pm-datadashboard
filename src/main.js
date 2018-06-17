91722138e3bc2e82935a15858b54820d8816b4db
nataliamh.88@gmail.com <nataliamh.88@gmail.com>
2018-06-17 14:44:39 -0400

    Funciona llamada de JSON!

--- src/main.js
+++ src/main.js
@@ -1,31 +1,28 @@
 //  USANDO FETCH!
-let cohortsContainer;
+let cohortsContainer = {};
 function descargarCohorts() {
   const loadingPage = document.getElementById('loading');// creo un div en html
   loadingPage.style = 'display: block;';
   // fetch me entrega una respuesta, un valor que esta en el futuro==> es una promesa
-  fetch('../data/cohorts/lim-2018-03-pre-core-pw/users.json').then( // then cuando recibo la respuesta, catch cuando carga
+  let url = ('https://natichan.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/users.json')
+  fetch(url).then( // then cuando recibo la respuesta, catch cuando carga
     (response) =>{ 
       if (response.ok) { // si la respuesta esta okay
         return response.json(); // este metodo responde con una promesa, es una promesa
-      } // puedo anidar promesas
+      } 
     }
   ).then( // cuando cumplo la promesa 
     // para hacer multiples llamadas, por ejemplo users, progress, courses, en la url va donde esta ubicada cada una
     (responseJson) =>{
-      const studentsReceptor = document.getElementById('usersContainer');
-      for (let index = 0; index < responseJson.length; index++) {
-        const students = document.createElement('div');
-        studentsReceptor.appendChild(students);      
-      } 
-    }
+      console.log(responseJson.users);   
+    }   
   ).then(
     (responseJson) =>{
       return fetch(responseJson.urldentrodelJson);
     }
   ).catch( // cuando la promesa falla
     (error) => {
-      console.log('Petición fallo');
+      console.log('Petición falló');
     }
   ); 
 }
