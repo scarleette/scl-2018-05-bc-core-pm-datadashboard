a8296aac1dfa38f19dffc5b488100bc0744bfa62
Scarlett <scar.antoine@gmail.com>
2018-06-18 12:19:13 -0400

    se guardan link con informacion de cohorts, progress, user por separado

---
+++ src/data.js
@@ -0,0 +1,3 @@
+// 'https://scarleette.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/users.json'
+//'https://scarleette.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/progress.json'
+//'https://scarleette.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts.json'
