0eac32e05f7d1d4bf68abe3e6bbdb95f0252d17b
Scarlett <scar.antoine@gmail.com>
2018-06-21 13:48:26 -0400

    se eliminan comentarios y se ordena codigo

--- src/data.js
+++ src/data.js
@@ -1,15 +1,21 @@
-var users = null;
-var progress = null;
-var cohorts = null;
+let users = [];
+let progress = {};
+let cohorts = [];
 
 fetch('https://natichan.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/users.json') // llamando a la data 
   .then(response => response.json()) // primera promesa, esperando llamada para dar ok
-  .then(dataUsers => renderUsers(dataUsers) // promesa para cuando se cumplio la llamada, el retorno es una variable que está en el main.
+  .then(dataUsers => {
+    users = dataUsers;
+    tabla(dataUsers);
+  } // promesa para cuando se cumplio la llamada, el retorno es una variable que está en el main.
   ).then(  
     fetch('https://natichan.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/progress.json')
       .then(responseTwo => responseTwo.json()
       ).then( 
-        (dataProgress) =>renderProgress(dataProgress)          
+        (dataProgress) => {
+          progress = dataProgress;
+          renderProgress(dataProgress)          
+        }
       ).then(
         fetch('https://natichan.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts.json')
           .then(responseThree => responseThree.json())
@@ -21,36 +27,3 @@ fetch('https://natichan.github.io/scl-2018-05-bc-core-pm-datadashboard/data/coho
           )
       ) 
   );
-
-
-// ##### Argumentos
-
-// * `options`: An object with the following keys:
-//   - `cohort`: Objeto cohort (de la lista de cohorts)
-//   - `cohortData`: Objeto con dos propiedades:
-//     + `users`: Arreglo de usuarios miembros del cohort.
-//     + `progress`: Objeto con data de progreso de cada usuario en el contexto de
-//       un cohort en particular.
-//   - `orderBy`: String con criterio de ordenado (ver `sortUsers`).
-//   - `orderDirection`: String con dirección de ordenado (ver `sortUsers`).
-//   - `search`: String de búsqueda (ver `filterUsers`)
-
-// ##### Valor de retorno
-
-// Nuevo arreglo de usuarios _ordenado_ y _filtrado_ con la propiedad `stats`
-// añadida (ver `computeUsersStats`).
-processCohortData = (options) =>{
-  return; 
-};
-
-computeUsersStats = (users, progress, courses) => {
-  return; 
-};
-
-sortUsers = (users, orderBy, orderDirection) => {
-  return; 
-};
-
-filterUsers = (users, search) =>{
-  return; 
-};
