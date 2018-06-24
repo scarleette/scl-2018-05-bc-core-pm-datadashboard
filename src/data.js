22ba2f3ec324843eeacc9445cf473b899c8c22cf
Scarlett <scar.antoine@gmail.com>
2018-06-24 16:43:18 -0400

    Corregir fetch

--- src/data.js
+++ src/data.js
@@ -1,35 +1,26 @@
 window.onload = () => {
   dataJson();
-}; dataJson = () => {
+}; 
+
+dataJson = () => {
   fetch('https://natichan.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/users.json') // llamando a la data 
-    .then(response => response.json()) // primera promesa, esperando llamada para dar ok
-    .then(dataUsers => {
-      users = dataUsers;
-      tabla(dataUsers);  
-    }
-    ).then(  
-      fetch('https://natichan.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/progress.json')
-        .then(responseTwo => responseTwo.json()
-        ).then( 
-          (dataProgress) => {
-            progress = dataProgress;       
-          }
-        ).then(
-          fetch('https://natichan.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts.json')
-            .then(responseThree => responseThree.json())
-            .then((dataCohorts) => {
-              cohorts = dataCohorts;
-              renderCohorts(dataCohorts);
-            }
-            ).catch( 
-              (error) => {
-                console.log('Petición falló');
-              }
-            )
-        ) 
-    );
+    .then((response) => {
+      response.json()
+        .then(dataUsers => {
+          fetch('https://natichan.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/progress.json')      
+            .then((response) => {
+              response.json()
+                .then(progress => {
+                  const dataComputed = computeUsersStats(dataUsers, progress);
+                  tabla(dataComputed);
+                })
+            })
+        });
+    })
+    .catch((error) => {
+      console.log('Petición falló');
+    })
 };
-  
 
 function computeUsersStats(dataAlumnas, dataProgress ) {
   for (let i = 0; i < dataAlumnas.length; ++i) {
